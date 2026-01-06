6e0bb24c5fe5c7704adad224b00184bf684f94b2
滑而不稽则罔 <139239270+huaerbujizewang@users.noreply.github.com>
2026-01-06 16:39:17 +0800

    Create list.js

---
+++ stories/list.js
@@ -0,0 +1,19 @@
+// stories/list.js
+const storyList = [
+    {
+        id: "clem_01",
+        title: "承诺的重量",
+        description: "关于克莱门汀、权力与那一栋尚未交付的宅邸。",
+        owner: "master@almorel.com", // 只有这个玩家能看
+        tags: "克莱门汀·瓦勒留斯",
+        date: "FR1494 - 2月"
+    },
+    {
+        id: "test_01",
+        title: "测试",
+        description: "这是一段只有 IDRotF 才能看到的剧情。",
+        owner: "IDRotF@1.com", 
+        tags: "系统测试",
+        date: "2026-01-01"
+    }
+];
